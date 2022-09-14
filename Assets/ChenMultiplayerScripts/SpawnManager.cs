f70172847f9fac588a92979610b2411caece23eb
Daimnon <49341329+Daimnon@users.noreply.github.com>
2022-09-14 15:47:17 +0300

    Things fixed

--- Assets/ChenMultiplayerScripts/SpawnManager.cs
+++ Assets/ChenMultiplayerScripts/SpawnManager.cs
@@ -18,10 +18,6 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private GameObject _patientMalePrefab;
     [SerializeField] private GameObject _patientFemalePrefab;//, _patientMaleOldPrefab, _patientFemaleOldPrefab, _patientKid, _patientToddler;
 
-    [Header("VehiclesPrefabs")]
-    //[SerializeField] private GameObject _ambulancePrefab;
-    [SerializeField] private GameObject _natanPrefab;
-
     [Header("GeneralPrefabs")]
     [SerializeField] private GameObject _crewRoomColliderPrefab;
     [SerializeField] private GameObject _eranRoomPrefab;
@@ -29,18 +25,14 @@ public class SpawnManager : MonoBehaviour
 
 
     [Header("Transform Positions In Scene")]
-    [SerializeField] private Transform _patientMalePosTransform;
-    [SerializeField] private Transform _patientFemalePosTransform;
     [SerializeField] private Transform _eranRoomPosTransform;
     [SerializeField] private Transform _operationRoomTransform;
-    [SerializeField] private Transform /*_ambulancePosTransform,*/ _natanPosTransform;
     [SerializeField] private Transform[] _crewRoomPosTransforms; // crew rooms collider positions in scene
 
     public float _minX, _minZ, _maxX, _maxZ;
 
     private void Awake()
     {
-
         if (Instance == null)
         {
             Instance = this;
@@ -53,17 +45,12 @@ public class SpawnManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(this.gameObject);
-        
     }
 
     void Start()
     {
         Vector3 randomPos = new Vector3(Random.Range(_minX, _maxX), 1.3f, Random.Range(_minZ, _maxZ));
         PhotonNetwork.Instantiate(_playerMalePrefab.name, randomPos, Quaternion.identity);
-
-        PhotonNetwork.InstantiateRoomObject(_patientMalePrefab.name, _patientMalePosTransform.position, _patientMalePrefab.transform.rotation);
-        //PhotonNetwork.InstantiateRoomObject(_patientFemalePrefab.name, _patientFemalePosTransform.position, _patientFemalePrefab.transform.rotation);
-
 
         if (PhotonNetwork.IsMasterClient)
         {
