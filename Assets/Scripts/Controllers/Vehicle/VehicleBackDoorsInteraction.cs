a3cc869072e3c5dfba9f69f209d215b08d63cad8
Daimnon <49341329+Daimnon@users.noreply.github.com>
2022-09-29 21:01:35 +0300

    Now Fixed

--- Assets/Scripts/Controllers/Vehicle/VehicleBackDoorsInteraction.cs
+++ Assets/Scripts/Controllers/Vehicle/VehicleBackDoorsInteraction.cs
@@ -7,6 +7,31 @@ public class VehicleBackDoorsInteraction : MonoBehaviour
 {
     [SerializeField] private int _barType;
 
+    private void CloseAllCurrentBarPanels(bool isNatan)
+    {
+        if (!isNatan)
+        {
+            UIManager.Instance.AmbulanceNoBagPanel.SetActive(false);
+            UIManager.Instance.AmbulanceAmbuPanel.SetActive(false);
+            UIManager.Instance.AmbulanceKidsAmbuPanel.SetActive(false);
+            UIManager.Instance.AmbulanceMedicPanel.SetActive(false);
+            UIManager.Instance.AmbulanceDefibrilationPanel.SetActive(false);
+            UIManager.Instance.AmbulanceOxygenPanel.SetActive(false);
+            UIManager.Instance.AmbulanceMonitorPanel.SetActive(false);
+        }
+        else
+        {
+            UIManager.Instance.NatanNoBagPanel.SetActive(false);
+            UIManager.Instance.NatanAmbuPanel.SetActive(false);
+            UIManager.Instance.NatanKidsAmbuPanel.SetActive(false);
+            UIManager.Instance.NatanMedicPanel.SetActive(false);
+            UIManager.Instance.NatanQuickDrugsPanel.SetActive(false);
+            UIManager.Instance.NatanDrugsPanel.SetActive(false);
+            UIManager.Instance.NatanOxygenPanel.SetActive(false);
+            UIManager.Instance.NatanMonitorPanel.SetActive(false);
+        }
+    }
+
     public void Interact()
     {
         for (int i = 0; i < ActionsManager.Instance.AllPlayersPhotonViews.Count; i++)
@@ -14,7 +39,14 @@ public class VehicleBackDoorsInteraction : MonoBehaviour
             if (ActionsManager.Instance.AllPlayersPhotonViews[i].IsMine)
             {
                 if (UIManager.Instance.CurrentActionBarParent.activeInHierarchy)
+                {
+                    if (UIManager.Instance.CurrentActionBarParent != UIManager.Instance.NatanBar)
+                        CloseAllCurrentBarPanels(false);
+                    else
+                        CloseAllCurrentBarPanels(true);
+
                     UIManager.Instance.CurrentActionBarParent.SetActive(false);
+                }
 
                 UIManager.Instance.CurrentActionBarParent = _barType switch
                 {
