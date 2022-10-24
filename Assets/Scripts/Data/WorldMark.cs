671cf3281ac576eaec28842a12577bad2e233ec0
Chen5454 <74614370+Chen5454@users.noreply.github.com>
2022-10-24 19:26:35 +0300

    Marker area - half fixed

--- Assets/Scripts/Data/WorldMark.cs
+++ Assets/Scripts/Data/WorldMark.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MarkerIcon { Icon1, Icon2, Icon3, Icon4, Icon5, Icon6}
+
 public class WorldMark : MonoBehaviour
 {
+    public MarkerIcon MarkerIconEnum;
+
     [SerializeField] private List<Sprite> _marks;
     public List<Sprite> Marks => _marks;
 }
