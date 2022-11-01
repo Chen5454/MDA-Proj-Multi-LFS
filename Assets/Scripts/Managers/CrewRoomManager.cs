786548dd67641924da227d85f82ccb69fba8205f
Daimnon <49341329+Daimnon@users.noreply.github.com>
2022-11-01 20:38:58 +0200

    puhspush

--- Assets/Scripts/Managers/CrewRoomManager.cs
+++ Assets/Scripts/Managers/CrewRoomManager.cs
@@ -223,7 +223,7 @@ public class CrewRoomManager : MonoBehaviour,IPunObservable
 
             //3) Instantiate correct prefab
 
-            GameObject go = PhotonNetwork.InstantiateRoomObject(_patientMale.name, GameManager.Instance.IncidentPatientSpawns[apartmentNum].position, GameManager.Instance.IncidentPatientSpawns[apartmentNum].rotation);
+            GameObject go = PhotonNetwork.Instantiate(_patientMale.name, GameManager.Instance.IncidentPatientSpawns[apartmentNum].position, GameManager.Instance.IncidentPatientSpawns[apartmentNum].rotation);
             //3.5) Grab the Patient component from the instantiated object.
             //4) Set this patients data to the NewPatientData to be spawned
             go.GetComponent<Patient>().InitializePatientData(PatientCreationSpace.PatientCreator.newPatient); 
