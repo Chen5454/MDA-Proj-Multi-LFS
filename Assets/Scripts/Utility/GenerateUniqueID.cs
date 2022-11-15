d25f3ceb0aa28046c508134d9785c572c793270b
AlonsHub <65736848+AlonsHub@users.noreply.github.com>
2022-11-15 19:36:00 +0200

    ID - just the class, and some thoughts

---
+++ Assets/Scripts/Utility/GenerateUniqueID.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GenerateUniqueID
+{
+   //INITIAL APPROACH
+   //Basically use DateTime - something that won't repeat
+}
