acbf24ecbbe9ea30d09bbc7baf803903c1227ccc
AlonsHub <65736848+AlonsHub@users.noreply.github.com>
2022-11-29 21:59:20 +0200

    Measurement fields
    Get measurement data with StatsPanel.UpdateMeasurement(MeasurmentsEnum or int)

--- Assets/Scripts/UI/StatsPanel.cs
+++ Assets/Scripts/UI/StatsPanel.cs
@@ -8,11 +8,48 @@ public class StatsPanel : MonoBehaviour
     [SerializeField]
     List<TMP_Text> textBoxs;
 
+    NewPatientData connectedPatient;
     public void SetMe(NewPatientData newPatientData)
     {
-        for (int i = 0; i < textBoxs.Count; i++)
+        if (textBoxs.Count != System.Enum.GetValues(typeof(Measurements)).Length)
         {
-            textBoxs[i].text = $"{(Measurements)i}: {newPatientData.GetMeasurement(i)}";
+            Debug.LogError("Different counts of measurements and measurement-fields!");
+            return;
         }
+
+        connectedPatient = newPatientData;
+
+        ////test
+        //SetAllFields();
     }
+
+    //private void SetAllFields()
+    //{
+    //    for (int i = 0; i < textBoxs.Count; i++)
+    //    {
+    //        UpdateField(i);
+    //    }
+    //}
+
+    public void UpdateMeasurement(int i)
+    {
+        textBoxs[i].text = $"{(Measurements)i}: {connectedPatient.GetMeasurement(i)}";
+    }
+    public void UpdateMeasurement(Measurements m)
+    {
+        textBoxs[(int)m].text = $"{m}: {connectedPatient.GetMeasurement((int)m)}";
+    }
+
+    private void SetFieldBlank(int i)
+    {
+        textBoxs[i].text = $"{(Measurements)i}: ";
+    }
+    //[ContextMenu("Set box text")]
+    //public void SetBoxText()
+    //{
+    //    for (int i = 0; i < textBoxs.Count; i++)
+    //    {
+    //        SetFieldBlank(i);
+    //    }
+    //}
 }
