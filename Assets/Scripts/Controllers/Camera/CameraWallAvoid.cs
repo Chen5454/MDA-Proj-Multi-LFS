4c8c1d1b6e69d6a6d5997922e6b8b8ddd6efa052
StuXet <36419721+StuXet@users.noreply.github.com>
2023-01-10 13:27:04 +0200

    Naming Convention T_T

--- Assets/Scripts/Controllers/Camera/CameraWallAvoid.cs
+++ Assets/Scripts/Controllers/Camera/CameraWallAvoid.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 
 public class CameraWallAvoid : MonoBehaviour
 {
-    [SerializeField] private Transform player;
-    [SerializeField] private Transform ogCameraPosition;
-    [SerializeField] private float height = 2f;
-    [SerializeField] private float smoothTime = .3f;
+    [SerializeField] private Transform _player;
+    [SerializeField] private Transform _ogCameraPosition;
+    [SerializeField] private float _height = 2f;
+    [SerializeField] private float _smoothTime = .3f;
     //[SerializeField] private float maxDistance = 3f;
-    [SerializeField] private LayerMask wallLayer;
-    [SerializeField] bool ignoreTrigger = true;
+    [SerializeField] private LayerMask _wallLayer;
+    [SerializeField] bool _ignoreTrigger = true;
 
-    private Vector3 velocity = Vector3.zero;
+    private Vector3 _velocity = Vector3.zero;
 
     void Update()
     {
         // Raycast from the camera to the player
         RaycastHit hit;
-        if (Physics.Linecast(transform.position, new Vector3(player.position.x, player.position.y + height, player.position.z), out hit, wallLayer, ignoreTrigger ? QueryTriggerInteraction.Ignore : QueryTriggerInteraction.UseGlobal))
+        if (Physics.Linecast(transform.position, new Vector3(_player.position.x, _player.position.y + _height, _player.position.z), out hit, _wallLayer, _ignoreTrigger ? QueryTriggerInteraction.Ignore : QueryTriggerInteraction.UseGlobal))
         {
             // If the raycast hits a wall, move the camera closer to the player
-            transform.position = Vector3.SmoothDamp(transform.position, hit.point, ref velocity, smoothTime);
+            transform.position = Vector3.SmoothDamp(transform.position, hit.point, ref _velocity, _smoothTime);
         }
         else
         {
             // The camera will return to its original position if raycast does not hit the wall
-            if (transform.position != ogCameraPosition.position)
+            if (transform.position != _ogCameraPosition.position)
             {
-                transform.position = Vector3.SmoothDamp(transform.position, ogCameraPosition.position, ref velocity, smoothTime);
+                transform.position = Vector3.SmoothDamp(transform.position, _ogCameraPosition.position, ref _velocity, _smoothTime);
             }
         }
     }
